fc09229a08251272871e862cf3130398026015f5
Mahdi <msar.abdullah.idn@gmail.com>
2026-02-13 22:03:24 +0800

    while gampang cuk

---
+++ while_loop/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 6;
+
+    /*while (syarat){
+        (aksi);
+    }*/
+
+    while (a < 10){
+        cout << "Pengulangan ke-" << a -5 << endl;
+        a++;
+    }
+
+    cout << "Selesai" << endl;
+    
+    cin.get();
+    return 0;
+}
